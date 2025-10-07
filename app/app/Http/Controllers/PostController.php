4c755dc8b1abe36ae2d34952dbb84e737bd9f1ce
Ivan-Stoykov <ivanxrist8@gmail.com>
2025-10-07 18:17:39 +0300

    added returning posts and creating post responces

---
+++ app/app/Http/Controllers/PostController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+use Symfony\Component\HttpFoundation\Response;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return response( $posts, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Post::create(['title'=>$request->input('title'),
+        'slug' => $request->input('slug'),
+        'content' => $request->input('content'),
+        'authorId' => $request->input('author.id'),
+        'publishedAt' => $request->input('publishedAt'),
+        'status' => $request->input('status')]);
+        
+        return response('Post was created', 201);
+        
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
